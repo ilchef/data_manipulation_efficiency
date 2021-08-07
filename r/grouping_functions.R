0a7a80c8f351108a1f7434de999b5319c862ad63
ilchef <thomasilchef@gmail.com>
2021-08-07 17:06:24 +1000

    g

--- r/grouping_functions.R
+++ r/grouping_functions.R
@@ -1,7 +1,22 @@
 # base r
-
+group_base <- function(df){
+        
+}
 # dplyr
+group_dplyr <- function(df){
+ df_out <- df %>%
+         group_by(randoms,PM.2.5.TEOM.ug.m3) %>%
+         summarise(count=n())
+}
 
 # datatable
+group_dt <- function(dt){
+        dt_out <- dt
+}
 
 # dtplyr
+group_base <- function(df){
+        df_out <- df %>%
+                group_by(randoms,PM.2.5.TEOM.ug.m3) %>%
+                summarise(count=n())
+}
